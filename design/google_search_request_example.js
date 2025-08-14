3ba0b68fec85a5d5b3f79c55082d669cc0a46b58
Mark Dierolf <mdierolf@cloudquote.io>
2025-08-14 15:52:41 -0700

    Normalize code styling and formatting in Google News and Search request examples.

--- design/google_search_request_example.js
+++ design/google_search_request_example.js
@@ -1,28 +1,27 @@
-const axios = require('axios');
+const axios = require("axios");
 let data = JSON.stringify({
- "q": "apple inc",
- "location": "United States"
+	q: "apple inc",
+	location: "United States",
 });
 
 let config = {
- method: 'post',
- maxBodyLength: Infinity,
- url: 'https://google.serper.dev/search',
- headers: {
-  'X-API-KEY': '170091cc0ee87094149f6e4f4879b822ad6ddcc7',
-  'Content-Type': 'application/json'
- },
- data : data
+	method: "post",
+	maxBodyLength: Infinity,
+	url: "https://google.serper.dev/search",
+	headers: {
+		"X-API-KEY": "170091cc0ee87094149f6e4f4879b822ad6ddcc7",
+		"Content-Type": "application/json",
+	},
+	data: data,
 };
 
 async function makeRequest() {
- try {
-  const response = await axios.request(config);
-  console.log(JSON.stringify(response.data));
- }
- catch (error) {
-  console.log(error);
- }
+	try {
+		const response = await axios.request(config);
+		console.log(JSON.stringify(response.data));
+	} catch (error) {
+		console.log(error);
+	}
 }
 
 makeRequest();
