b2c24088ac11e5c8473e143d882a0fbf442e02cc
Mark Dierolf <mdierolf@cloudquote.io>
2025-12-18 13:53:40 -0800

    Extract and modularize plugin logic into `plugin.ts`

---
+++ plugin.ts
@@ -0,0 +1,26 @@
+import TokenRingApp from "@tokenring-ai/app";
+import {TokenRingPlugin} from "@tokenring-ai/app";
+import {WebSearchConfigSchema, WebSearchService} from "@tokenring-ai/websearch";
+import packageJSON from './package.json' with {type: 'json'};
+import SerperWebSearchProvider, {SerperWebSearchProviderOptionsSchema} from "./SerperWebSearchProvider.js";
+
+
+export default {
+  name: packageJSON.name,
+  version: packageJSON.version,
+  description: packageJSON.description,
+  install(app: TokenRingApp) {
+    const websearchConfig = app.getConfigSlice("websearch", WebSearchConfigSchema);
+
+    if (websearchConfig) {
+      app.waitForService(WebSearchService, cdnService => {
+        for (const name in websearchConfig.providers) {
+          const provider = websearchConfig.providers[name];
+          if (provider.type === "serper") {
+            cdnService.registerProvider(name, new SerperWebSearchProvider(SerperWebSearchProviderOptionsSchema.parse(provider)));
+          }
+        }
+      });
+    }
+  },
+} as TokenRingPlugin;
